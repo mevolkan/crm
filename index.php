a1587edfe7b7f65069f8cf951bf6d438ce68681d
volkan <nzaro19@gmail.com>
2022-10-17 13:10:13 +0300

    redo method

--- index.php
+++ index.php
@@ -1,16 +1,41 @@
-$url = 'http://server.com/path';
-$data = array('key1' => 'value1', 'key2' => 'value2');
+<?php
+// Your ID and token
+$blogID = '8070105920543249955';
+$authToken = 'OAuth 2.0 token here';
 
-// use key 'http' even if you send the request to https://...
-$options = array(
-    'http' => array(
-        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-        'method'  => 'POST',
-        'content' => http_build_query($data)
-    )
+// The data to send to the API
+$postData = array(
+    'kind' => 'blogger#post',
+    'blog' => array('id' => $blogID),
+    'title' => 'A new post',
+    'content' => 'With <b>exciting</b> content...'
 );
-$context  = stream_context_create($options);
-$result = file_get_contents($url, false, $context);
-if ($result === FALSE) { /* Handle error */ }
 
-var_dump($result);
+// Setup cURL
+$ch = curl_init('https://www.googleapis.com/blogger/v3/blogs/'.$blogID.'/posts/');
+curl_setopt_array($ch, array(
+    CURLOPT_POST => TRUE,
+    CURLOPT_RETURNTRANSFER => TRUE,
+    CURLOPT_HTTPHEADER => array(
+        'Authorization: '.$authToken,
+        'Content-Type: application/json'
+    ),
+    CURLOPT_POSTFIELDS => json_encode($postData)
+));
+
+// Send the request
+$response = curl_exec($ch);
+
+// Check for errors
+if($response === FALSE){
+    die(curl_error($ch));
+}
+
+// Decode the response
+$responseData = json_decode($response, TRUE);
+
+// Close the cURL handler
+curl_close($ch);
+
+// Print the date from the response
+echo $responseData['published'];
