2550367127fba67b5d26855ac46eafc2ce09ca3e
volkan <16579273+mevolkan@users.noreply.github.com>
2022-10-25 15:58:12 +0300

    test curl

---
+++ test2.php
@@ -0,0 +1,22 @@
+<?php
+$url = "http://www.example.org/";
+$ch = curl_init();
+curl_setopt ($ch, CURLOPT_URL, $url);
+curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, 5);
+curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
+$contents = curl_exec($ch);
+if (curl_errno($ch)) {
+  echo curl_error($ch);
+  echo "\n<br />";
+  $contents = '';
+} else {
+  curl_close($ch);
+}
+
+if (!is_string($contents) || !strlen($contents)) {
+echo "Failed to get contents.";
+$contents = '';
+}
+
+echo $contents;
+?>    
