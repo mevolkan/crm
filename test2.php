6703bb5eaa3cdd969ebbad20c8a52203d90cf990
volkan <16579273+mevolkan@users.noreply.github.com>
2022-11-11 09:29:32 +0300

    test data

--- test2.php
+++ test2.php
@@ -3,13 +3,19 @@ include 'creds.php';
 
 $url = "https://jumla.cic.co.ke/api/Auth/Login";
 $ch = curl_init();
+$certificate_location = "/certificate/cacert.pem";
 curl_setopt ($ch, CURLOPT_URL, $url);
+
+curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, $certificate_location);
+curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, $certificate_location);
+// curl_setopt ($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+
 curl_setopt ($ch, CURLOPT_POST, 1);                //0 for a get request
 curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, 5);
 curl_setopt ($ch, CURLOPT_TIMEOUT, 20);
 curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt( $ch, CURLOPT_POSTFIELDS, $credsData );
-curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+curl_setopt ($ch, CURLOPT_POSTFIELDS, $credsData );
+curl_setopt ($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
 $contents = curl_exec($ch);
 if (curl_errno($ch)) {
   echo curl_error($ch);
