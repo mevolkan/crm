875380752cade12ddba2602de0d09dbf2a5987d4
volkan <16579273+mevolkan@users.noreply.github.com>
2022-11-11 09:35:24 +0300

    temp

--- test2.php
+++ test2.php
@@ -5,11 +5,9 @@ $url = "https://jumla.cic.co.ke/api/Auth/Login";
 $ch = curl_init();
 curl_setopt ($ch, CURLOPT_URL, $url);
 curl_setopt ($ch, CURLOPT_POST, 1);                //0 for a get request
-curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, 5);
-curl_setopt ($ch, CURLOPT_TIMEOUT, 20);
 curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt( $ch, CURLOPT_POSTFIELDS, $credsData );
-curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+curl_setopt ($ch, CURLOPT_POSTFIELDS, $credsData );
+curl_setopt ($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
 $contents = curl_exec($ch);
 if (curl_errno($ch)) {
   echo curl_error($ch);
